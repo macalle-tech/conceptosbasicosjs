b4cf2e7af4a1f0b253c593e2c4fbe0a572189f48
Mario Andres Calle <ma.calle@unitecnica.net>
2026-02-06 20:49:48 -0500

    mi primer ejemplo

--- ejemplo.js
+++ ejemplo.js
@@ -1,1 +1,14 @@
 console.log("hola mi primer ejemplo js");
+
+let edad = 30;
+let nombre, apellido;
+
+nombre = "mario"
+apellido = "calle";
+
+const telefono = "3117351221";
+
+
+nombre = "carlos";
+
+console.log(nombre, "tiene", edad, "años y su telefono es", telefono);
